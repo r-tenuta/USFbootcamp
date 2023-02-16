643797cb9e5c932f9d09be02c2736b81e18401dd
r-tenuta <118233133+r-tenuta@users.noreply.github.com>
2023-02-16 11:32:19 -0500

    Create let_and_const_exercise.js

---
+++ let_and_const_exercise.js
@@ -0,0 +1,10 @@
+const pi = 3.14
+pi = 42; //error
+
+
+// Keyword	Can Reassign	Can Redeclare	Can Mutate	Scope Rules
+// var	          yes        	yes	        yes	      function scope
+// let	          yes	        no	        yes	      block scope
+// const	        no	        no	        yes	      block scope
+
+// Hoisting is a mechanism in JavaScript that allows variables and functions to be declared after they are used in the code. In other words, JavaScript "hoists" these declarations to the top of their respective scopes, which means that they are available to the entire scope, regardless of where they are declared in the code.
